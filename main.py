19ff7b87a0a78fffd8e366bb7df90c0c1993c500
João Pedro G. C <115590969+jpgercc@users.noreply.github.com>
2023-12-30 13:59:32 -0300

    main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ i = 160  # ????
 time.sleep(1)
 
 while True:
-    pyautogui.moveTo(x=1800, y=265)  # altere estes valores rodando o arquivo test.py
+    pyautogui.moveTo(x=1800, y=265)  # change the values according to your screen using test.py file
     pyautogui.click()
     time.sleep(100)
     pyautogui.moveTo(x=1800, y=265 + i)
